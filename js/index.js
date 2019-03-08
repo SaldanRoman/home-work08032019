cc290b9392a999b2b619b588ac5f2f0076d66503
SaldanRoman <34034422+SaldanRoman@users.noreply.github.com>
2019-03-08 03:50:24 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,45 @@
 const display = document.querySelector('.display');
-document.querySelectorAll('.digits button, .opers button').forEach(button => button.addEventListener('click', digitPressed));
+let mms;
+document.querySelectorAll('.digits button, .opers button').forEach(button=>button.addEventListener('click', digitPressed));
 
-function digitPressed(e){
+function digitPressed(e) {
     display.value += e.target.innerHTML;
 }
 
-document.querySelectorAll('.equal').forEach(button => button.addEventListener('click', equalPressed));
+document.querySelectorAll('.equal').forEach(button=>button.addEventListener('click', equalPressed));
 
-function equalPressed(){
+function equalPressed() {
     display.value = eval(display.value);
 }
 
-document.querySelectorAll('.clear').forEach(button => button.addEventListener('click', clearPressed));
+document.querySelectorAll('.clear').forEach(button=>button.addEventListener('click', clearPressed));
 
-function  clearPressed(){
+function clearPressed() {
     display.value = "";
+
 }
 
-document.querySelectorAll('.sqrt').forEach(button => button.addEventListener('click', sqrtPressed));
+document.querySelectorAll('.sqrt').forEach(button=>button.addEventListener('click', sqrtPressed));
 
-function  sqrtPressed(){
+function sqrtPressed() {
     display.value = Math.sqrt(eval(display.value));
+}
+
+document.querySelectorAll('.memsave').forEach(button=>button.addEventListener('click', memsavePressed));
+
+function memsavePressed() {
+    mms = display.value;
+
+}
+
+document.querySelectorAll('.memclear').forEach(button=>button.addEventListener('click', memclearPressed));
+
+function memclearPressed() {
+    mms = "";
+}
+
+document.querySelectorAll('.memrec').forEach(button=>button.addEventListener('click', memrecPressed));
+
+function memrecPressed() {
+    display.value += mms;
 }
